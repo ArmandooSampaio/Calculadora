dbda352044420f99d0fbe0ca5cb7a1101cd32ba1
Armando Sampaio <armandoosampai0@gmail.com>
2024-02-05 17:41:45 -0300

    criando as operações no js

---
+++ javascript.js
@@ -0,0 +1,13 @@
+let num1 = 0;
+let num2 = 0;
+let operation = "";
+
+const add = (a,b) => a + b;
+
+const subtract = (a,b) => a - b;
+
+const mutiply = (a,b) => a * b;
+
+const divide = (a, b) => a / b;
+
+
